d6f0e069625c4048741a08c93b4e1ec5f6ff62e5
Genar Trias Ortiz <genar@acs.li>
2016-05-27 09:00:39 +0200

    adding service declaration

--- Security/TokenAuthenticator.php
+++ Security/TokenAuthenticator.php
@@ -2,7 +2,14 @@
 
 namespace Cirici\JWTClientBundle\Security;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Doctrine\ORM\EntityManager;
 
 /**
  * Token Authenticator.

14787bb75051be2cecf38a2930067b0dd024b293
Genar Trias Ortiz <genar@acs.li>
2016-06-30 18:30:32 +0200

    trying to test the bundle as standalone

--- Tests/App/bootstrap.php
+++ Tests/App/bootstrap.php
@@ -2,7 +2,15 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require __DIR__.'/../../../../../vendor/autoload.php';
+$file = __DIR__.'/../../vendor/autoload.php';
+if (!file_exists($file))
+{
+    $file = __DIR__.'/../../../../../../vendor/autoload.php';
+    if (!file_exists($file))
+        throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+$loader = require $file;
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 

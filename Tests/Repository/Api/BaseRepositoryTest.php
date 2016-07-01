b94482fcf93daaec7238fffb0e7ac16b4def0529
Genar Trias Ortiz <genar@acs.li>
2016-07-01 12:13:26 +0200

    conditional kernel loading and droping symfony 2.3 tests

--- Tests/Repository/Api/BaseRepositoryTest.php
+++ Tests/Repository/Api/BaseRepositoryTest.php
@@ -8,7 +8,16 @@ class BaseRepositoryTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        require_once __DIR__.'/../../App/AppKernel.php';
+        $file = __DIR__.'/../../App/AppKernel.php';
+        if (!file_exists($file))
+        {
+            $file = __DIR__.'/../../../../../../app/AppKernel.php';
+            if (!file_exists($file))
+                throw new RuntimeException('No kernel found.');
+        }
+
+        require_once $file;
+
         $kernel = new \AppKernel('test', true);
         $kernel->boot();
         $this->container = $kernel->getContainer();
